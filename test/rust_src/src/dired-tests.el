3efc3328975fe8a4a032339b6fcaa5df02170349
Glenn Barry <gaak99@gaak99.com>
2018-06-14 18:26:01 -0400

    Rust port of file-attributes from dired.c (lessp new test)

--- test/rust_src/src/dired-tests.el
+++ test/rust_src/src/dired-tests.el
@@ -4,6 +4,12 @@
 
 (require 'ert)
 
+(ert-deftest test-file-attributes-lessp ()
+  (should-error (eval '(file-attributes-lessp "rms" "wilfred")) :type 'wrong-type-argument)
+  (should-error (eval '(file-attributes-lessp '("rms") "wilfred")) :type 'wrong-type-argument)
+  (should (eq t (file-attributes-lessp '("rms") '("wilfred"))))
+  (should (eq nil (file-attributes-lessp '("wilfred") '("rms")))))
+
 (ert-deftest test-system-users ()
   (should-error (eval '(system-users 'rms)) :type 'wrong-number-of-arguments)
   ;; The result should be a list of >= 1 user name(s) on all Unix and GNU systems.
