f03a616b343ab899882b296f9d290f01d8407b55
Glenn Barry <gaak99@gaak99.com>
2018-07-07 17:50:44 -0400

    quick fix and cleanup

--- src/dired.c
+++ src/dired.c
@@ -332,7 +332,7 @@ directory_files_internal (Lisp_Object directory, Lisp_Object full,
 	      /*gb*/
 #ifdef WINDOWSNT
 	      Lisp_Object fileattrs
-		= file_attributes (fd, dp->d_name, directory, name, id_format);
+		= file_attributes_static (fd, dp->d_name, directory, name, id_format);
 #else
 	      Lisp_Object fileattrs
 		= file_attributes_rust_internal (directory, name, id_format);
@@ -885,57 +885,6 @@ stat_gname (struct stat *st)
 
 /*gb*/
 #ifdef WINDOWSNT
-/* DEFUN ("file-attributes", Ffile_attributes, Sfile_attributes, 1, 2, 0,
-       doc: Return a list of attributes of file FILENAME.
-Value is nil if specified file cannot be opened.
-
-ID-FORMAT specifies the preferred format of attributes uid and gid (see
-below) - valid values are `string' and `integer'.  The latter is the
-default, but we plan to change that, so you should specify a non-nil value
-for ID-FORMAT if you use the returned uid or gid.
-
-To access the elements returned, the following access functions are
-provided: `file-attribute-type', `file-attribute-link-number',
-`file-attribute-user-id', `file-attribute-group-id',
-`file-attribute-access-time', `file-attribute-modification-time',
-`file-attribute-status-change-time', `file-attribute-size',
-`file-attribute-modes', `file-attribute-inode-number', and
-`file-attribute-device-number'.
-
-Elements of the attribute list are:
- 0. t for directory, string (name linked to) for symbolic link, or nil.
- 1. Number of links to file.
- 2. File uid as a string or a number.  If a string value cannot be
-  looked up, a numeric value, either an integer or a float, is returned.
- 3. File gid, likewise.
- 4. Last access time, as a list of integers (HIGH LOW USEC PSEC) in the
-  same style as (current-time).
-  (See a note below about access time on FAT-based filesystems.)
- 5. Last modification time, likewise.  This is the time of the last
-  change to the file's contents.
- 6. Last status change time, likewise.  This is the time of last change
-  to the file's attributes: owner and group, access mode bits, etc.
- 7. Size in bytes.
-  This is a floating point number if the size is too large for an integer.
- 8. File modes, as a string of ten letters or dashes as in ls -l.
- 9. An unspecified value, present only for backward compatibility.
-10. inode number.  If it is larger than what an Emacs integer can hold,
-  this is of the form (HIGH . LOW): first the high bits, then the low 16 bits.
-  If even HIGH is too large for an Emacs integer, this is instead of the form
-  (HIGH MIDDLE . LOW): first the high bits, then the middle 24 bits,
-  and finally the low 16 bits.
-11. Filesystem device number.  If it is larger than what the Emacs
-  integer can hold, this is a cons cell, similar to the inode number.
-
-On most filesystems, the combination of the inode and the device
-number uniquely identifies the file.
-
-On MS-Windows, performance depends on `w32-get-true-file-attributes',
-which see.
-
-On some FAT-based filesystems, only the date of last access is recorded,
-so last access time will always be midnight of that day.  )*/
-
 Lisp_Object
 file_attributes_c(Lisp_Object filename, Lisp_Object id_format)
 {
