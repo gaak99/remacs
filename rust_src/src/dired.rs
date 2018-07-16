515d730238af0f4f9d9b9f2309ce59f51d3de674
Glenn Barry <gaak99@gaak99.com>
2018-07-16 19:47:40 -0400

    Rust port of dired's file-attributes (unfix filename suffix)

--- rust_src/src/dired.rs
+++ rust_src/src/dired.rs
@@ -1,9 +1,9 @@
 //! Lisp functions for making directory listings.
 
 #[cfg(unix)]
-include!("dired_unix.rs");
+include!("dired_unix.rs__");
 
 #[cfg(windows)]
-include!("dired_windows.rs");
+include!("dired_windows.rs__");
 
 include!(concat!(env!("OUT_DIR"), "/dired_exports.rs"));
